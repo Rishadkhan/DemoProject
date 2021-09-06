2f06cde7b1e1814c137ad9f0606d2e732dfdb01f
Risahdkhan <rishadkhan670@gmail.com>
2021-09-06 17:33:08 +0600

    This is first commit on developer

--- first.py
+++ first.py
@@ -1,1 +1,11 @@
-print('Rishad')
+n = int(input(''))
+if n == 1:
+    print('January')
+elif n == 2:
+    print('February')
+elif n == 3:
+    print('March')
+elif n == 4:
+    print('April')
+elif n == 5:
+    print('May')
