8b9e17098b89db305a798f83e8d5aacbef222751
Risahdkhan <rishadkhan670@gmail.com>
2021-09-07 15:41:24 +0600

    some change

--- first.py
+++ first.py
@@ -1,13 +1,7 @@
-n = int(input(''))
+n = int(input())
 if n == 1:
     print('January')
 elif n == 2:
     print('February')
 elif n == 3:
     print('March')
-elif n == 4:
-    print('April')
-elif n == 5:
-    print('May')
-elif n == 6:
-    print('June')
