ccf703572d778deee831bbdfe13fe76f6ca45e93
Risahdkhan <rishadkhan670@gmail.com>
2021-09-07 15:43:37 +0600

    change some file

---
+++ first.py
@@ -0,0 +1,11 @@
+n = int(input())
+if n == 1:
+    print('January')
+elif n == 2:
+    print('February')
+elif n == 3:
+    print('March')
+elif n == 4:
+    print('April')
+elif n == 5:
+    print('May')
