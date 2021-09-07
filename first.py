da65d6ed5be7f528b23cc9b61c48c4ab900003f0
Risahdkhan <rishadkhan670@gmail.com>
2021-09-07 15:30:01 +0600

    commit

--- first.py
+++ first.py
@@ -11,15 +11,3 @@ elif n == 5:
     print('May')
 elif n == 6:
     print('June')
-elif n == 7:
-    print('July')
-elif n == 8:
-    print('August')
-elif n == 9:
-    print('September')
-elif n == 10:
-    print('October')
-elif n == 11:
-    print('November')
-elif n == 12:
-    print('December')
